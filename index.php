244d1d9b3ff4bd4c497d0f29d2feb6d163ee42b2
banz-ghb <banz.ggl@gmail.com>
2013-02-17 10:55:48 +0900

    candidate url in order to view timeline

--- index.php
+++ index.php
@@ -393,6 +393,9 @@ $app_name = idx($app_info, 'name', '');
       <h1>Check Your Facebook Timeline</h1>
       <ul>
         <li>
+          <!-- response.username -->
+          <!-- https://www.facebook.com/akinori.kohno.5/allactivity?privacy_source=activity_log&log_filter=app_554694347877002 -->
+          <!-- https://www.facebook.com/${RESPONSE.USERNAME}/allactivity?privacy_source=activity_log&log_filter=app_${APPID} -->
           <a href="https://www.heroku.com/?utm_source=facebook&utm_medium=app&utm_campaign=fb_integration" target="_top" class="icon apps-on-facebook">Timeline</a>
           <p>View the activity logs of lislog in your facebook timeline.</p>
         </li>
