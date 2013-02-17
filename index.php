75ab715ae922ccffe458b6cb50a5bc3ac713b045
banz-ghb <banz.ggl@gmail.com>
2013-02-17 12:24:02 +0900

    <div class="fb-login-button" data-scope="user_likes,user_photos" scope="publish_actions"></div>

--- index.php
+++ index.php
@@ -321,7 +321,8 @@ $app_name = idx($app_info, 'name', '');
       <?php } else { ?>
       <div>
         <h1>Welcome</h1>
-        <div class="fb-login-button" data-scope="user_likes,user_photos"></div>
+        <!-- https://developers.facebook.com/docs/reference/plugins/login/ -->
+        <div class="fb-login-button" data-scope="user_likes,user_photos" scope="publish_actions"></div>
       </div>
       <?php } ?>
     </header>
