8003b994350e7ce218484b0a543013bac273f85d
banz-ghb <banz.ggl@gmail.com>
2013-03-09 18:53:45 +0900

    test

--- index.php
+++ index.php
@@ -135,6 +135,7 @@ $app_name = idx($app_info, 'name', '');
       //samples
       //get-started
       $("#menu-most-recent-activity").click(function(){ //menu function 1 start
+          alert(test);
           //http://stackoverflow.com/questions/7193425/how-do-you-animate-fb-canvas-scrollto?answertab=active#tab-top
           $('html,body').animate(
             {scrollTop: $("#most-recent-activity").offset().top},
