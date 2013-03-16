295505c94ebfff1c40fc09cd96ce527544a08770
banz-ghb <banz.ggl@gmail.com>
2013-03-16 10:00:00 +0900

    change word

--- index.php
+++ index.php
@@ -317,7 +317,7 @@ $app_name = idx($app_info, 'name', '');
       <!-- 2013-03-03 banz-ghb start no extended permission when logging in  data-scope="user_likes,user_photos,publish_actions" -->
       <!-- Refer to https://developers.facebook.com/docs/reference/plugins/login/ -->
       <div id="fb-login" class="fb-login-button" data-show-faces="true"></div>
-      <div id="fb-auth" >アプリを承認する</div>
+      <div id="fb-auth" >リスログを使用する</div>
       <!--div id="fb-auth" class="fb-login-button" data-scope="user_likes,user_photos,publish_actions"--><!--/div-->
       <!-- 2013-03-03 banz-ghb end   no extended permission when logging in-->
     </header>
