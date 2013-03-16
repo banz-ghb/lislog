ab4da4c3e1e14454067c5faf28679060b1681fa9
banz-ghb <banz.ggl@gmail.com>
2013-03-16 11:19:02 +0900

    guide scroll

--- index.php
+++ index.php
@@ -150,18 +150,19 @@ $app_name = idx($app_info, 'name', '');
       //about-us
       //get-started
       //$("#menu-get-started").click(function(){ //menu function 1 start
-      function testclick(){
+      //function testclick(){
+      $("#navi-guide").click(function(){ //menu function 1 start
           //alert(test);
           //http://stackoverflow.com/questions/7193425/how-do-you-animate-fb-canvas-scrollto?answertab=active#tab-top
           $('html,body').animate(
-            {scrollTop: $("#get-started").offset().top},
+            {scrollTop: $("#guide").offset().top},
             {duration: 1000, step: function(top_offset){
               FB.Canvas.scrollTo(0, top_offset + 30);
             }
           });
           return false;
           //alert("debug2: "+top_offset);
-      }//); //menu function 1 end
+      }); //menu function 1 end
 
       //////////////////////////////////////////////////////////////
       //View functions
@@ -281,9 +282,9 @@ $app_name = idx($app_info, 'name', '');
     <div id="navigation"><!-- style="height:50px;border:1px solid blue;" -->
       <div id="navigation_top" class="clearfix">
         <ul>
-          <li><a id="#Top"               >トップ</a></li>
-          <li><a id="#MostRecentActivity">アクティビティログ</a></li>
-          <li><a id="#Guide"             >ガイド</a></li>
+          <li><a id="navi-top"               >トップ</a></li>
+          <li><a id="navi-most-recent-sctivity">アクティビティログ</a></li>
+          <li><a id="navi-guide"             >ガイド</a></li>
         </ul>
       </div>
     </div>
